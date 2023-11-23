da7b31b9d33271cf9b99583cbb8f722452cf7b53
Nelson Molina <geovannysangucho25@gmail.com>
2023-11-23 12:03:16 -0500

    Hola mundo

---
+++ TiendaProductoOnline/TiendaProductoOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TiendaProductoOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
